4d30728353acef27ae06ccc818b6e2d386148fe7
Andrew Mukiza <andrewmukiza@gmail.com>
2014-04-08 08:37:53 +0300

    more comprehensive feature test for post listing page

--- features/homepage.feature
+++ features/homepage.feature
@@ -2,4 +2,7 @@ Feature: Posts
     Scenario: List posts
         Given I have ten posts
         And I visit the home page
+        Then I should see the welcome text
+        And The page should have title
+        And I should see the navigation links
         Then I should see the posts listed
--- features/step_definitions/home_steps.rb
+++ features/step_definitions/home_steps.rb
@@ -1,6 +1,7 @@
 Given(/^I visit the home page$/) do
     visit "/"
 end
+
 Given(/^I have ten posts$/) do
     (1..10).to_a.each{|index|
         @post = Post.new :title => "post %d" % index, :content => "content %d" % index
@@ -12,5 +13,19 @@ Then(/^I should see the posts listed$/) do
     (1..10).to_a.each{|index|
         expect(page).to have_content "post %d" % index
         expect(page).to have_content "content %d" % index 
+    }
+end
+
+Then(/^I should see the welcome text$/) do
+    expect(page).to have_content "Welcome to my blog"
+end
+
+Then(/^The page should have title$/) do
+    expect(page).to have_title "::MyBlog"
+end
+
+Then(/^I should see the navigation links$/) do
+    ["Posts", "About", "Contact"].each{|link|
+        expect(page).to have_content link
     }
 end
